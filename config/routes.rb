012d96916d684487a950bcec60a7d85d2581577c
YutoKuroda-git <prog101jp@gmail.com>
2026-01-28 14:40:27 +0900

    Deviseを用いたログイン／ログアウト機能とログイン状態に応じたヘッダー切り替えを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "reservations/index"
+  get "rooms/index"
+  get "users/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +14,10 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  root "rooms#index"
+
+  resource :user, only: [ :show ]
+  resources :rooms
+  resources :reservations
 end
