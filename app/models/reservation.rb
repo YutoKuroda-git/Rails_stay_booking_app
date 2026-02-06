5598b6f9773ea8c26189429789d2a43f3073b371
YutoKuroda-git <prog101jp@gmail.com>
2026-02-06 21:21:10 +0900

    施設予約機能と予約一覧ページを実装

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,33 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :check_in_date, :check_out_date, :guest_count, presence: true
+  validates :guest_count, numericality: { greater_than_or_equal_to: 1 }
+  validate :check_in_date_is_today_or_later
+  validate :check_out_date_is_after_check_in
+
+  def stay_days
+    (check_out_date - check_in_date).to_i
+  end
+
+  def total_price
+    room.price_per_night * stay_days * guest_count
+  end
+
+  private
+
+  def check_in_date_is_today_or_later
+    return if check_in_date.blank?
+    if check_in_date < Date.today
+      errors.add(:check_in_date, "本日以降の日付を選択してください")
+    end
+  end
+
+  def check_out_date_is_after_check_in
+    return if check_in_date.blank? || check_out_date.blank?
+    if check_out_date <= check_in_date
+      errors.add(:check_out_date, "チェックイン日より後の日付を選択してください")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "reservations/index"
-  get "rooms/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -30,5 +28,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :reservations
+  resources :rooms do
+    resources :reservations, only: [ :index, :new, :create ] do
+      collection do
+        post :confirm
+    end
+  end
+end
+  resources :reservations, only: [ :index ]
 end
