0140a4c8d37b57da1e2881e89df7171439119bcc
YutoKuroda-git <prog101jp@gmail.com>
2026-02-08 15:04:46 +0900

    マイグレーション修正：不要なカラムを削除

--- db/migrate/20260123061653_create_rooms.rb
+++ db/migrate/20260123061653_create_rooms.rb
@@ -2,7 +2,6 @@ class CreateRooms < ActiveRecord::Migration[7.2]
   def change
     create_table :rooms do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :image
       t.string :name
       t.text :description
       t.integer :price_per_night
--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_31_232427) do
 
   create_table "rooms", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "image"
     t.string "name"
     t.text "description"
     t.integer "price_per_night"
@@ -65,12 +64,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_31_232427) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password_digest"
     t.string "icon"
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
